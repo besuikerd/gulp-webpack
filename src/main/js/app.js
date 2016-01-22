26d9cfec66bd276f5745056ee5dd8d6302345b8e
Nick ten Veen <nveen0930@gmail.com>
2016-01-22 12:11:10 +0100

    added react preset

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,4 +1,13 @@
 require('./globals');
 require('../styles/style.scss');
 
-document.getElementById('content').innerHTML = "Hello world!";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloWorld extends React.Component{
+  render(){
+    return <div> {this.props.text}</div>
+  }
+}
+
+ReactDOM.render(<HelloWorld text="Hello World"/>, document.getElementById('content'));
