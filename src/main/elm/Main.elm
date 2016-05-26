f039ab11fef97c68818d8d37233c779fd44926c2
Nick ten Veen <nveen0930@gmail.com>
2016-05-26 11:58:45 +0200

    Added some boilerplate to Main.elm

--- src/main/elm/Main.elm
+++ src/main/elm/Main.elm
@@ -1,8 +1,36 @@
-import Html exposing (..)
+module Main exposing (..)
 
-main : Html a
-main =
-  div
-    []
-    [ text "Hello World!"
-    ]
+import Html exposing (..)
+import Html.App as App exposing (..)
+
+main : Program Never
+main = App.program
+  { init = init
+  , update = update
+  , view = view
+  , subscriptions = subscriptions
+  }
+
+type Msg
+  = NoOp
+
+type alias Model =
+  {
+  }
+
+init : (Model, Cmd Msg)
+init =
+  ( Model
+  , Cmd.none
+  )
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NoOp -> (model, Cmd.none)
+
+view : Model -> Html Msg
+view model = div [] [ text "Hello from Elm" ]
